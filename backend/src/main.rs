3314710a8d49afeca2e1fc5aa60603b27e12a875
Saksham <saksham1387@gmail.com>
2026-01-21 01:01:10 +0530

    Added health endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -17,6 +17,10 @@ use routes::user::{create_user,sign_in,get_user};
 use crate::game::AppState;
 use crate::routes::room::{create_room, get_rooms};
 
+async fn health_check() -> Result<web::Json<String>> {
+    Ok(web::Json("OK".to_string()))
+}
+
 #[actix_web::main]
 async fn main() -> Result<()> {
     let app_state = web::Data::new(AppState::new());
@@ -34,6 +38,7 @@ async fn main() -> Result<()> {
         actix_web::App::new()
         .wrap(cors)
         .service(web::scope("/api/v1")
+            .service(web::resource("/health").route(web::get().to(health_check)))
             .service(web::resource("/signup").route(web::post().to(create_user)))
             .service(web::resource("/signin").route(web::post().to(sign_in)))
             .service(web::resource("/me").route(web::get().to(get_user)))
